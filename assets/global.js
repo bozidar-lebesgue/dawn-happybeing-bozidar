92b6daf159e7ee9f4613d2e117eecea3968756c9
bozidar-lebesgue <bozidar@lebesgue.io>
2023-12-04 18:06:06 +0100

    Fixed some styling

--- assets/global.js
+++ assets/global.js
@@ -205,6 +205,71 @@ class QuantityInput extends HTMLElement {
 
 customElements.define('quantity-input', QuantityInput);
 
+
+class QuantityInputHb extends HTMLElement {
+  constructor() {
+    super();
+    this.input = this.querySelector('input');
+    this.changeEvent = new Event('change', { bubbles: true });
+    this.input.addEventListener('change', this.onInputChange.bind(this));
+    this.querySelectorAll('button').forEach((button) =>
+      button.addEventListener('click', this.onButtonClick.bind(this))
+    );
+  }
+
+  quantityUpdateUnsubscriber = undefined;
+
+  connectedCallback() {
+    this.validateQtyRules();
+    this.quantityUpdateUnsubscriber = subscribe(PUB_SUB_EVENTS.quantityUpdate, this.validateQtyRules.bind(this));
+  }
+
+  disconnectedCallback() {
+    if (this.quantityUpdateUnsubscriber) {
+      this.quantityUpdateUnsubscriber();
+    }
+  }
+
+  onInputChange(event) {
+    this.validateQtyRules();
+    const value = this.input.value;
+    this.querySelectorAll('button').forEach((button) =>
+      button.classList.remove('quantity__selected')
+    );
+    this.querySelector(`.quantity__button[name='${value}']`).classList.add('quantity__selected');
+  }
+
+  onButtonClick(event) {
+    event.preventDefault();
+    const previousValue = this.input.value;
+
+    if (event.target.name === 'plus') {
+      this.input.stepUp();
+    } else if (event.target.name === 'minus') {
+      this.input.stepDown()
+    } else {
+      this.input.value = parseInt(event.target.name)
+    };
+    if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+  }
+
+  validateQtyRules() {
+    const value = parseInt(this.input.value);
+    if (this.input.min) {
+      const min = parseInt(this.input.min);
+      const buttonMinus = this.querySelector(".quantity__button[name='minus']");
+      buttonMinus.classList.toggle('disabled', value <= min);
+    }
+    if (this.input.max) {
+      const max = parseInt(this.input.max);
+      const buttonPlus = this.querySelector(".quantity__button[name='plus']");
+      buttonPlus.classList.toggle('disabled', value >= max);
+    }
+  }
+}
+
+customElements.define('quantity-input-hb', QuantityInputHb);
+
 function debounce(fn, wait) {
   let t;
   return (...args) => {
