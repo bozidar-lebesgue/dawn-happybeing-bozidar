a5e500c51ab875926cf51c0a8ddbf28a6f1b5f3f
bozidar-lebesgue <bozidar@lebesgue.io>
2023-12-11 21:52:23 +0100

    Quantity selector

--- assets/global.js
+++ assets/global.js
@@ -210,9 +210,7 @@ class QuantityInputHb extends HTMLElement {
   constructor() {
     super();
     this.input = this.querySelector('input');
-    this.changeEvent = new Event('change', { bubbles: true });
-    this.input.addEventListener('change', this.onInputChange.bind(this));
-    this.querySelectorAll('button').forEach((button) =>
+    this.querySelectorAll('.quantity__option').forEach((button) =>
       button.addEventListener('click', this.onButtonClick.bind(this))
     );
   }
@@ -230,42 +228,17 @@ class QuantityInputHb extends HTMLElement {
     }
   }
 
-  onInputChange(event) {
-    this.validateQtyRules();
-    const value = this.input.value;
-    this.querySelectorAll('button').forEach((button) =>
-      button.classList.remove('quantity__selected')
-    );
-    this.querySelector(`.quantity__button[name='${value}']`).classList.add('quantity__selected');
-  }
-
   onButtonClick(event) {
     event.preventDefault();
-    const previousValue = this.input.value;
-
-    if (event.target.name === 'plus') {
-      this.input.stepUp();
-    } else if (event.target.name === 'minus') {
-      this.input.stepDown()
-    } else {
-      this.input.value = parseInt(event.target.name)
-    };
-    if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
+    this.input.value = parseInt(event.target.dataset.quantity);
+    
+    this.querySelectorAll('.quantity__option').forEach((button) =>
+      button.classList.remove('quantity--selected')
+    );
+    this.querySelector(`.quantity__option[data-quantity='${this.input.value}']`).classList.add('quantity--selected');
   }
 
-  validateQtyRules() {
-    const value = parseInt(this.input.value);
-    if (this.input.min) {
-      const min = parseInt(this.input.min);
-      const buttonMinus = this.querySelector(".quantity__button[name='minus']");
-      buttonMinus.classList.toggle('disabled', value <= min);
-    }
-    if (this.input.max) {
-      const max = parseInt(this.input.max);
-      const buttonPlus = this.querySelector(".quantity__button[name='plus']");
-      buttonPlus.classList.toggle('disabled', value >= max);
-    }
-  }
+  validateQtyRules() {}
 }
 
 customElements.define('quantity-input-hb', QuantityInputHb);
@@ -1297,36 +1270,13 @@ customElements.define('variant-radios', VariantRadios);
 
 class CustomTabs extends HTMLElement {
   constructor() {
-    console.log('##blabla');
     super();
     this.tabs = this.querySelectorAll('.custom-tab');
     this.tabContents = this.querySelectorAll('.custom__tab-content');
 
-    console.log('##', 'asd2', this.tabs.length);
-
     this.querySelectorAll('.custom-tab').forEach((tab) =>
       tab.addEventListener('click', this.onButtonClick.bind(this))
     );
-
-    
-
-    // tabs.forEach(function(tab) {
-    //   tab.addEventListener('click',function() {
-    //       console.log('asd')
-    //       tabContents.forEach(function(content){
-    //           content.classList.remove('active');
-    //       });
-
-    //       tabs.forEach(function(tab){
-    //           tab.classList.remove('active');
-    //       });
-
-    //       console.log(tab.classList)
-
-    //       this.classList.add('active');
-    //       // content.classList.add('active');
-    //   });
-    // });
   }
 
   onButtonClick(event) {
@@ -1343,14 +1293,7 @@ class CustomTabs extends HTMLElement {
 
     let contentId = event.target.dataset.contentId;
     document.getElementById(contentId).classList.add('active');
-    // content.classList.add('active');
-
-
-    // event.target.name === 'plus' ? this.input.stepUp() : this.input.stepDown();
-    // if (previousValue !== this.input.value) this.input.dispatchEvent(this.changeEvent);
   }
-
-
 }
 
 customElements.define('custom-tabs', CustomTabs);
